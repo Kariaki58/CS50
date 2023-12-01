32820b24779ab4642c75ca3cc154dc485826b857
kariaki58 <kariakichernocherno@gmail.com>
2023-12-01 03:03:02 +0000

    condition in cs50

---
+++ Lecture1-C/condition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 5;
+	int y = 2;
+
+	if (x < y)
+		printf("x is less than y\n");
+	else if (x > y
+		printf("x is greater than y\n");
+	else
+		printf("x is not less than y\n");
+	return (0);
+}
